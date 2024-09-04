8e919a8ebc7eb6e3a6e5661583fdc1fd1751054a
BSSE23107 <143435795+BSSE23107@users.noreply.github.com>
2024-09-04 19:30:57 +0500

    basic setup
    json setup
    main cpp

---
+++ main.cpp
@@ -0,0 +1,26 @@
+//
+// Created by azhar on 04/09/2024.
+//
+#include <iostream>
+#include "paths.json"
+#include "/nlohmann/json.hpp"
+
+using namespace std;
+
+int main(){
+    int choice;
+    cout<<"Welcome to ticket Generation System\n"
+          "Please Specify Your Role\n"
+          "1. Customer\n"
+          "2. Others\n"
+          "   ";
+    switch (choice) {
+        case 1:
+            break;
+        case 2:
+            break;
+
+
+    }
+    return 0;
+}
